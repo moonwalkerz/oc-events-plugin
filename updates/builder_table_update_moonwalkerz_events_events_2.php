06f14543fce5a94bf72c0dfca5540d8c92ce34d8
Alain Martini <webmaster@inalto.com>
2022-10-19 16:03:47 -0400

    Added the ability to mark as canceled an event

---
+++ updates/builder_table_update_moonwalkerz_events_events_2.php
@@ -0,0 +1,23 @@
+<?php namespace MoonWalkerz\Events\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateMoonwalkerzEventsEvents2 extends Migration
+{
+    public function up()
+    {
+        Schema::table('moonwalkerz_events_events', function($table)
+        {
+            $table->boolean('canceled')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('moonwalkerz_events_events', function($table)
+        {
+            $table->dropColumn('canceled');
+        });
+    }
+}
