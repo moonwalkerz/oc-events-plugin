62babd940ad73dd6402cff29dd87f50806d47d14
MoonWalkerz <nicolo@fiveupmedia.it>
2022-10-24 21:27:58 +0200

    dependency fixes

--- Plugin.php
+++ Plugin.php
@@ -4,6 +4,9 @@ use System\Classes\PluginBase;
 
 class Plugin extends PluginBase
 {
+
+    public $require = ['RainLab.Translate'];
+
     public function registerComponents()
     {
         return [
